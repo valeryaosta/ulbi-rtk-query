9cb111607c36aa8a09043fd6155d55154c14f16a
valeryaosta <gerasimenko.valery@gmail.com>
2021-11-06 00:49:51 +0300

    base logic rtk

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from "./hooks/redux";
-import {userSlice} from "./store/reducers/UserSlice";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
 
-    const {count} = useAppSelector(state => state.userReducer)
-    const {increment} = userSlice.actions
     const dispatch = useAppDispatch()
+    const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
 
     return (
         <div className="App">
-            <h1>{count}</h1>
-            <button onClick = {() => dispatch(increment(10))}>INCREMENT</button>
+            {isLoading && <h1>Идет загрузка...</h1>}
+            {error && <h1>{error}</h1>}
+            {JSON.stringify(users, null, 2)}
+
         </div>
     );
 }
